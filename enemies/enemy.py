4b5f7cb2a95e5b0e4383948a97b372679b11fa50
Miroslav Savel <miroslav.savel@gmail.com>
2022-04-28 15:48:09 +0200

    run game with background - pygame.error: video system not initialized

--- enemies/enemy.py
+++ enemies/enemy.py
@@ -2,9 +2,12 @@
 
 class Enemy:
     imgs = []
-    def __init__(self, x, y):
+    def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
+        # for collisions
+        self.width = width
+        self.height = height
         self.animation_count = 0
         self.health = 1
         self.path = []
@@ -18,15 +21,22 @@ class Enemy:
         """
         self.animation_count += 1
         self.img = self.imgs[self.animation_count]
+        # reset animation
+        if self.animation_count > len(self.imgs):
+            self.animation_count = 0
         win.blit(self.img, (self.x, self.y))
+        self.move()
 
-    def collide(self, x, y):
+    def collide(self, X, Y):
         """
         Returns if position has hit enemy
         :param x:
         :param y:
         :return:
         """
+        if X < self.x + self.width and X >= self.x:
+            if Y <= self.y + self.height and Y >= self.y:
+                return True
         return False
 
     def move(self):
--- game.py
+++ game.py
@@ -1,4 +1,7 @@
 import pygame
+import os
+
+pygame.init()
 
 class Game:
     def __init__(self):
@@ -9,13 +12,32 @@ class Game:
         self.towers = []
         self.lives = 10
         self.money = 100
+        self.bg = pygame.image.load(os.path.join("assets/background", "background.jpg"))
+        self.clicks = []
 
     def run(self):
         run = True
-
+        clock = pygame.time.Clock()
         while run:
+            clock.tick(60)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
 
+            pos = pygame.mouse.get_pos()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.clicks.append(pos)
+                print(pos)
+            self.draw()
             pygame.quit()
+
+    def draw(self):
+        self.win.blit(self.bg, (0,0))
+        for p in self.clicks:
+            pygame.draw.circle(self.win, (255,0,0), (p[0], p[1]), 5, 0)
+        pygame.display.update()
+
+
+g = Game()
+g.run()
